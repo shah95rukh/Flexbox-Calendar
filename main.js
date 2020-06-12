66940c5cba5e760b759d1e55a33fbea1534ecb59
Syed Ahmed <shah_rukh95@hotmail.com>
2020-06-11 21:05:03 -0400

    Mobile responsiveness added, dropdown added

--- main.js
+++ main.js
@@ -56,13 +56,26 @@ function truncateString(str, num) {
 console.log(truncateString(longString, 10));
 console.log(truncateString(longString, 20));
 
+var subtractValue;
+var i = 0;
+
 //Going to see what the bounding rectangle of the day names are
+// hardcoded way: subtract 22 because getBoundingClientRect doesn't
+// get the correct value in the first try
 function adjustHeight() {
     var rect = $("#dayNames")[0].getBoundingClientRect();
-    winHeight = $(window).height() - rect.bottom;
-    $(".flex-container > div").css({
-        height: winHeight / 6,
-    });
+    i == 0 ? (subtractValue = 22) : (subtractValue = 0);
+    winHeight = $(window).height() - rect.bottom - subtractValue;
+    if ($(".flex-container > div").css("flex-basis") == "13%") {
+        $(".flex-container > div").css({
+            height: winHeight / x,
+        });
+    } else {
+        $(".flex-container > div").css({
+            height: "auto",
+        });
+    }
+    i++;
 }
 
 function buildCalendar(change) {
@@ -80,25 +93,35 @@ function buildCalendar(change) {
         } else {
             visibleMonth -= 1;
         }
+    } else {
+        if (Number.isInteger(change)) {
+            visibleMonth = change;
+        }
     }
     currentDaysInMonth = daysInMonth(visibleMonth, visibleYear);
     currentDayOffset = dayOffset(visibleMonth, visibleYear);
-
     $(".monthName").text(months[visibleMonth] + "  " + visibleYear);
+
     //First let's get the dayNames DIV
     var dayNamesDiv = document.getElementById("dayNames");
     dayNamesDiv.innerHTML = "";
-    //Now we create a for loop that builds the content
+
     for (var i = 0; i < weekdays.length; i++) {
-        //Don't forget to use += to add to the content rather than replace it
         dayNamesDiv.innerHTML += "<div>" + weekdays[i] + "</div>";
     }
+
+    // Dropdown months
+    var dropdownDiv = document.getElementById("dropdown-c");
+    dropdownDiv.innerHTML = "";
+    for (var i = 0; i < months.length; i++) {
+        dropdownDiv.innerHTML +=
+            `<div onclick="buildCalendar(${i})">` + months[i] + "</div>";
+    }
+    console.log(dropdownDiv);
 
     var allFlex = $(".flex-container");
     //Now we create a for loop that builds the content
     var myCount = 1;
-    // var daysOffset = document.getElementById("daysOffset").value;
-    // var numberOfDaysToShow = document.getElementById("numDaysToShow").value;
     const copyDaysOffset = currentDayOffset;
 
     for (var i = 0; i < allFlex.length; i++) {
@@ -108,7 +131,7 @@ function buildCalendar(change) {
         for (var j = 0; j < 7; j++) {
             if (myCount > currentDaysInMonth || currentDayOffset > 0) {
                 allFlex[i].innerHTML +=
-                    "<div style='visibility: hidden'>" + myCount + "</div>";
+                    "<div class='offsetDivs'>" + myCount + "</div>";
                 currentDayOffset--;
             } else {
                 var div = "<div>";
@@ -118,9 +141,21 @@ function buildCalendar(change) {
                     currentYear == visibleYear
                 ) {
                     div =
-                        '<div style="background-color: orange; color: white">';
+                        "<div style='background-color: orange; color: white'>";
                 }
-                allFlex[i].innerHTML += div + myCount + "</div>";
+                allFlex[i].innerHTML +=
+                    div +
+                    "<div class='first-row'>" +
+                    "<div>" +
+                    myCount +
+                    "</div>" +
+                    "<div>" +
+                    weekdays[j] +
+                    "</div>" +
+                    "</div>" +
+                    "<div>Birthday re..</div>" +
+                    "<div>Dancing class</div>" +
+                    "</div>";
                 myCount++;
             }
         }
@@ -130,12 +165,15 @@ function buildCalendar(change) {
         !(copyDaysOffset == 5 && currentDaysInMonth > 30)
     ) {
         $("#lastDiv").css({ display: "none" });
+        x = 5.5;
     } else {
         $("#lastDiv").css({ display: "flex" });
+        x = 6.7;
     }
 
     adjustHeight();
-    $(window).resize(function () {
-        adjustHeight();
-    });
 }
+
+$(window).resize(function () {
+    adjustHeight();
+});
